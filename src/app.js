db6489cfac8f54c1f72d0479007e0595f17b6bda
cseemannr <camilaseemannramos@gmail.com>
2023-10-18 17:13:04 -0300

    HTML and CSS done. API request from Input value

--- src/app.js
+++ src/app.js
@@ -1,8 +1,50 @@
-import { apiKey } from "./key";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiKey}`;
+let formCityEl = document.querySelector("#form-city");
+let inputEl = document.querySelector("#text-input");
+let clearButtonEl = document.querySelector("#form-clear");
+let headingEl = document.querySelector("h1");
+let dateTimeEl = document.querySelector("#date-time");
+let fahrenheitEl = document.querySelector("#temperature-fahrenheit-link");
+let celciusEl = document.querySelector("#temperature-celcius-link");
+let temperatureEl = document.querySelector("#temperature");
+let iconEl = document.querySelector("#icon");
+let humidityEl = document.querySelector("#humidity");
+let windyEl = document.querySelector("#windy");
+
+let daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let now = new Date();
+let currentDay = daysOfWeek[now.getDay()];
+let currentHours = now.getHours();
+let currentMinutes = now.getMinutes();
+let currentTemp = 32;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = inputEl.value;
+
+  if (city) {
+    let apiKey = "303adf0028cb2b7e4cf7863cb1943e4e";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
+    axios.get(apiUrl).then(renderTemperature);
+  }
+}
 
 function renderTemperature(res) {
   console.log(res.data);
 }
 
-axios.get(apiUrl).then(renderTemperature);
+function clearForm() {
+  inputEl.value = ``;
+}
+
+clearButtonEl.addEventListener("click", clearForm);
+
+formCityEl.addEventListener("submit", handleSubmit);
