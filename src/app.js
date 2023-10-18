3b3a81d080b48b3ed3308559521813a3dbea740d
cseemannr <camilaseemannramos@gmail.com>
2023-10-18 13:44:23 -0300

    API key hidden

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-let apiKey = "303adf0028cb2b7e4cf7863cb1943e4e";
+import { apiKey } from "./key";
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiKey}`;
 
 function renderTemperature(res) {
